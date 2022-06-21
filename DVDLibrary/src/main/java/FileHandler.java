28647aa0f4a61cef45e60ccb3eb74b0a0f523f5d
Alicia Fisher <aliciajadefisher@yahoo.co.uk>
2022-06-21 14:57:29 +0100

    11 - FileHandler commented

--- DVDLibrary/src/main/java/FileHandler.java
+++ DVDLibrary/src/main/java/FileHandler.java
@@ -3,19 +3,37 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
+/**
+ * Class which deals with writing to and reading from a file
+ */
 public class FileHandler
 {
     String fileName;
 
+    /**
+     * Main constructor
+     * @param fileName - name of the file to be used
+     * @throws IOException
+     * @throws ParseException
+     */
     public FileHandler(String fileName) throws IOException, ParseException
     {
         this.fileName = fileName;
         createFile();
     }
 
+    /**
+     * Method which deals with file creation
+     * @throws IOException
+     * @throws ParseException
+     */
     public void createFile() throws IOException, ParseException
     {
         File file = new File(fileName);
+
+        // Checks if a file exits
+        // If it does, then it reads from it
+        // If it doesn't, then it creates one
         if(file.exists())
         {
             readFromFile();
@@ -26,6 +44,11 @@ public class FileHandler
         }
     }
 
+    /**
+     * Method which writes a hashMap to file
+     * @param library - hashMap of DVDs
+     * @throws IOException
+     */
     public void writeToFile(HashMap<Integer, DVD> library) throws IOException
     {
         PrintWriter writer = new PrintWriter(new FileWriter(fileName));
@@ -40,10 +63,18 @@ public class FileHandler
         writer.close();
     }
 
+    /**
+     * Method which reads every line from a file
+     * @return library - hashMap of DVDs
+     * @throws FileNotFoundException
+     * @throws ParseException
+     */
     public HashMap<Integer, DVD> readFromFile() throws FileNotFoundException, ParseException
     {
         HashMap<Integer, DVD> library = new HashMap<>();
 
+        // Loops through each line of the file
+        // Splits each line into it's parts and saves them as a DVD object, before putting the object into the HashMap
         if(new File(fileName).length() != 0)
         {
             Scanner reader = new Scanner(new BufferedReader(new FileReader(fileName)));
