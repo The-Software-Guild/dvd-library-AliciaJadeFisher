005db18b4c67917ebbe520ebabb3ab2bb5259f60
Alicia Fisher <aliciajadefisher@yahoo.co.uk>
2022-06-21 15:01:32 +0100

    11 - DVDLibrary commented

--- DVDLibrary/src/main/java/DVDLibrary.java
+++ DVDLibrary/src/main/java/DVDLibrary.java
@@ -3,49 +3,90 @@ import java.io.IOException;
 import java.text.ParseException;
 import java.util.HashMap;
 
+/**
+ * Class which represents the DVD Library
+ */
 public class DVDLibrary
 {
     int indexNotFound = -99;
+
+    // Attributes of DVDLibrary
     HashMap<Integer,DVD> library;
     FileHandler handler;
 
+    /**
+     * Main constructor
+     * @throws IOException
+     * @throws ParseException
+     */
     public DVDLibrary() throws IOException, ParseException
     {
         handler = new FileHandler("DVDLibrary.txt");
         readLibrary();
     }
 
+    /**
+     * Retrieves the library of DVDs
+     * @return library
+     */
     public HashMap<Integer,DVD> getLibrary()
     {
         return library;
     }
 
+    /**
+     * Uses the FileHandler to read the file
+     * @throws FileNotFoundException
+     * @throws ParseException
+     */
     public void readLibrary() throws FileNotFoundException, ParseException
     {
         library = handler.readFromFile();
     }
 
+    /**
+     * Uses the FileHandler to write to the file
+     * @throws IOException
+     */
     public void saveLibrary() throws IOException
     {
         handler.writeToFile(library);
     }
 
+    /**
+     * Adds a new DVD to the library
+     * @param dvd - dvd object input from the user
+     */
     public void addDVD(DVD dvd)
     {
        int index = library.keySet().size();
        library.put(index, dvd);
     }
 
+    /**
+     * Deletes a DVD from the library
+     * @param index - library index input from the user
+     */
     public void deleteDVD(int index)
     {
         library.remove(index);
     }
 
+    /**
+     * Updates a DVD in the library
+     * @param index - library index input from the user
+     * @param dvd - dvd object input from the user
+     */
     public void editDvd(int index, DVD dvd)
     {
         library.put(index,dvd);
     }
 
+    /**
+     * Searches the library for dvd based on the title
+     * @param title - title input from the user
+     * @return - dvd object if found, null if not found
+     */
     public DVD searchDvd(String title)
     {
         for(DVD dvd : library.values())
@@ -57,18 +98,6 @@ public class DVDLibrary
         }
 
         return null;
-    }
-
-    public int getIndex(DVD dvd)
-    {
-        for(int i : library.keySet())
-        {
-            if(library.get(i).equals(dvd))
-            {
-                return i;
-            }
-        }
-        return indexNotFound;
     }
 
 }
